f2150ab425b53a5fc2cf3778b0f714d9939a3824
Kr0pe <156374345+Kr0pe@users.noreply.github.com>
2025-01-13 18:22:57 +0000

    Defining_Motors_JV_Code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,13 +4,26 @@
 
 package frc.robot.subsystems;
 
+import java.lang.management.MemoryType;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {}
 
+  public final CANSparkMax leftDrivePrimaryMotor = new CANSparkMax(Constants.OperatorConstants.LEFT_DRIVE_PRIMARY_MOTOR_ID,MotorType.kBrushless);
+  public final CANSparkMax rightDrivePrimaryMotor = new CANSparkMax(Constants.OperatorConstants.RIGHT_DRIVE_PRIMARY_MOTOR_ID,MotorType.kBrushless);
+  public final CANSparkMax leftDriveFollowMotor = new CANSparkMax(Constants.OperatorConstants.RIGHT_DRIVE_PRIMARY_MOTOR_ID,MotorType.kBrushless);
+  public final CANSparkMax rightDriveFollowMotor = new CANSparkMax(Constants.OperatorConstants.RIGHT_DRIVE_FOLLOW_MOTOR_ID,MotorType.kBrushless);
+  
+  
+  
   /**
    * Example command factory method.
    *
